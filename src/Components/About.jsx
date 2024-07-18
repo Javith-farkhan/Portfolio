b77a7a9c053e9c5143eca7cdd9370e72ac321976
Javith farkhan. S <mohdjavith2003@gmail.com>
2024-07-18 09:32:23 +0530

    first commit

---
+++ src/Components/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const About = () => (
+  <section id="about">
+    <center>
+        <h2>About Me</h2>
+    </center>
+    <ul>
+<li>Hello!! I am Javith farkhan , a final-year student currently pursuing Artificial Intelligence & Machine Science in Saveetha Engineering College, CHENNAI.</li>
+<li>I Did my schooling in ST.Annes.Matric Hr.Sec.School ,Sivagangai.</li>
+<li>I have scored 83% in State Board Examination and currently with a CGPA of 7.8 as of semester-6.</li>
+<li>I am intrested in C,Python,C#,MySql,Database,HTML,CSS,React and Javascript. </li>
+<li>I Did  internship in Axis global Limited on Industrial Robotics. </li>
+<li>Languages known are English, Tamil</li>
+</ul>
+  </section>
+);
+
+export default About;
+
+
+
+
---
+++ src/Components/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Contact = () => (
+  <section id="contact">
+    <h2>Contact</h2>
+    <ul>
+<li><a href="mailto:your-email@example.com">Javith@gmail.com</a></li>
+<li><a href="mailto:your-email@example.com">www.linkedin.com/in/Javith/</a></li>
+<li><a href="mailto:your-email@example.com">github.com/Javith</a></li>
+<li>Contact: 9876543210</li>
+</ul>
+  </section>
+);
+export default Contact;
+
+
+
+
+
+
+
